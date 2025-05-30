93f5d3c1958d783304db913a3b285b90e1c76e21
y0ner <yonerisaza9@gmail.com>
2025-05-30 17:59:23 -0500

    30-05-2025 a las 5:59pm

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+# management/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import User, Product, Promotion, Table, Order, OrderDetail
+
+# La siguiente línea le dice a Django que muestre el modelo User en el panel de admin.
+admin.site.register(User)
+
+# Hacemos lo mismo para el resto de nuestros modelos.
+admin.site.register(Product)
+admin.site.register(Promotion)
+admin.site.register(Table)
+admin.site.register(Order)
+admin.site.register(OrderDetail)

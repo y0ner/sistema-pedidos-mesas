d12fd9910a5d9cc744acae5eb96a0d7f81e13d95
y0ner <yonerisaza9@gmail.com>
2025-06-08 20:01:40 -0500

    se arreglo el siderbard

--- FRONTEND/src/app/components/staff/staff-layout/staff-layout.ts
+++ FRONTEND/src/app/components/staff/staff-layout/staff-layout.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, RouterOutlet } from '@angular/router'; // Importar RouterModule y RouterOutlet
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../../services/auth';
 
 @Component({
   selector: 'app-staff-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, RouterOutlet], // Añadir RouterModule y RouterOutlet
+  imports: [CommonModule, RouterModule, RouterOutlet],
   templateUrl: './staff-layout.html',
   styleUrls: ['./staff-layout.css']
 })
 export class StaffLayoutComponent {
+  
+  isStaffMenuOpen = false;
 
   constructor(private authService: AuthService) { }
+
+  toggleStaffMenu(): void {
+    this.isStaffMenuOpen = !this.isStaffMenuOpen;
+  }
 
   logout(): void {
     this.authService.logout();
